338b8444fafa904641298c3687bab554c97d0e53
Raymond Ward <rayward1337@gmail.com>
2021-10-17 13:09:40 +0100

    added provided files

---
+++ Markov.java
@@ -0,0 +1,44 @@
+
+import java.lang.Math;
+import java.util.*;
+import java.io.PrintWriter;
+
+class Markov {
+
+    // ##################################
+
+    public static double getTransProb(int i, int j, int k) {
+
+        return 0.1;
+
+    }
+
+    // ##################################
+
+    public static double getSejProb(int s1, int s2, int numStates, double TS) {
+
+        return 0.1;
+
+    }
+
+    // ##################################
+    public static double getBiasTransProb(int s1, int s2, double[] ssprob) {
+
+        return 0.1;
+
+    }
+
+    // ##################################
+
+    public static double getContTransProb(int s1, int s2, double[] rates) {
+
+        return 0.1;
+
+    }
+
+    public static double getContSejProb(int s1, int s2, double[] rates, double TSC) {
+
+        return 0.1;
+    }
+
+}// end class
